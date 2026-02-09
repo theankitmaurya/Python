f385124a0bc03512d777987f56f05f007ae66bf5
Ankit <ankitmaurya20223@gmail.com>
2026-02-09 17:02:57 +0530

    Day 7 Updated

--- Day 7/CalculatorUsingPython.py
+++ Day 7/CalculatorUsingPython.py
@@ -18,7 +18,7 @@ Operator | Operator Name | Example
 a = 12
 b = 45
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
+print("The value of", a, "+", b, "is:",a + b)
+print("The value of", a, "-", b, "is:",a - b)
+print("The value of", a, "*", b, "is:",a * b)
+print("The value of", a, "/", b, "is:",a / b)
