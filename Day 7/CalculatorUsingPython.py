68276eb61a851920422881c9b39b06bfd8c2f69f
Ankit <ankitmaurya20223@gmail.com>
2026-02-09 16:50:45 +0530

    Day 7 completed

---
+++ Day 7/CalculatorUsingPython.py
@@ -0,0 +1,24 @@
+"""
+Operators - Python has different types of different operations.
+
+Arithmetic Operators -
+Operator | Operator Name | Example
++        | Addition      | 15 + 7
+-        | Subtraction   | 15 - 7
+*        | Multiplication| 15 * 7
+/        | Division      | 15 / 7
+**       | Exponential   | 15 ** 7
+%        | Modulus       | 15 % 7
+//       | Floor Division| 15 // 7
+"""
+
+# Exercise 1: Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program  should format the output in a readable manner. 
+
+
+a = 12
+b = 45
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
