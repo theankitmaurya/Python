eeb5171b44cab3439414ad1c80bbce7dd2740a17
Ankit <ankitmaurya20223@gmail.com>
2026-02-13 13:23:25 +0530

    Day 27 Completed

---
+++ Day 27/KaunBanegaCrorepati.py
@@ -0,0 +1,5 @@
+"""
+Create a program capable of displaying questions to the user like KBC.
+Use list data types to store the questions and their correct answers.
+Display the final amount the person is taking home after playing the game.
+"""
