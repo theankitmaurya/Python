1935715fdab7a054f3da1e2b3eacbb1af8fe7a33
Ankit <ankitmaurya20223@gmail.com>
2026-02-14 13:18:48 +0530

    Day 40 Completed

---
+++ Day 40/SecretCodeLanguage.py
@@ -0,0 +1,12 @@
+"""
+Write a python program to translate a message into secret code language. Use the rules below to translate normal English into secret code language.
+
+Rules:
+Coding ->
+1. If the word contains at least 3 characters, remove the first letter and append it at the end. Now append three random characters at the starting and the end.
+2. Else simply reverse the string.
+
+Decoding ->
+1. If the word contains less than 3 characters, reverse it.
+2. Else remove 3 random characters from start and end. now remove the last letter and append it to the beginning
+"""
